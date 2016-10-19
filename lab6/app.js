915f29d715759d9a32175933d98f6bb6f628d875
mahmoud-PC\mahmoud <mahmoud_m.elkhouly@yahoo.com>
2016-10-19 03:50:16 -0500

    lab1
    get Product list

--- lab6/app.js
+++ lab6/app.js
@@ -1,44 +1,10 @@
-var express = require('express');
-var bodyParser=require('body-parser');
-var fs = require('fs');
+var http = require('http');
+var fs = require ('fs')
 
-var app = express();
-app.set('view engine','ejs');
+http.createServer(function (req,res){
+  if (req.url=='/'){
+    res.writeHead(200,{'Conntent-Type':"plain/text"})
+     fs.createReadStream(__dirname+'/output.txt').pipe(res)
 
-
-
-
-//app.set('views', __dirname + 'views');
-
-//express.set('/assets',express.static('assets'))
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-app.get('/',function (req,res){
-
-   res.render('index');
-});
-
-app.get('/contactus',function (req,res){
-
-
-  res.render('contactus')
-
-
-});
-
-app.post('/contactus',urlencodedParser,function (req,res){
- console.log(req.body)
- //alert("Thank you");
-   var myWrite =fs.createWriteStream(__dirname+'/output.txt');
-   myWrite.write(JSON.stringify(req.body));
-
-  res.render('contactus')
-
-
-});
-
-
-
-app.listen(3000,'127.0.0.1');
-
-console.log(' server is running on port 3000');
+  }
+}).listen(3000,'127.0.0.1')
